32ef4ac99a3c155289f637be299b1b9713fb9b5e
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2025-08-29 19:19:05 +0200

    feat: initial addition of changelog integration

--- docs/src/content.config.ts
+++ docs/src/content.config.ts
@@ -1,7 +1,18 @@
-import { defineCollection } from 'astro:content';
-import { docsLoader } from '@astrojs/starlight/loaders';
-import { docsSchema } from '@astrojs/starlight/schema';
+import { defineCollection } from "astro:content";
+import { docsLoader } from "@astrojs/starlight/loaders";
+import { docsSchema } from "@astrojs/starlight/schema";
+import { changelogsLoader } from "starlight-changelogs/loader";
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  changelogs: defineCollection({
+    loader: changelogsLoader([
+      {
+        provider: "changeset",
+        base: "changelog",
+        changelog: "../CHANGELOG.md",
+        title: "Releases",
+      },
+    ]),
+  }),
 };
